7e188cd6510d419d816c73efb8d4fe205d2eede7
Jhonatan Silva <jhonatansilva@id.uff.br>
2023-02-03 03:08:04 -0300

    finished

--- js/index.js
+++ js/index.js
@@ -4,10 +4,13 @@ const pokemonImage = document.querySelector('.pokemon_image');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
 
+let searchPokemon = 1
 
 const fetchPokemons = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     if(APIResponse.status === 200) {
         const data = await APIResponse.json();
         return data;
@@ -15,14 +18,41 @@ const fetchPokemons = async (pokemon) => {
 }
 
 const renderPokemon = async (pokemon) => {
+    
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemons(pokemon);
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    input.value = '';
+    if (data){
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+        searchPokemon = data.id;
+    }else{
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Ops! Not found';
+        pokemonNumber.innerHTML = '';
+
+    }
+
 }
 
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
-    renderPokemon(input.value)
+    renderPokemon(input.value.toLowerCase())
 });
+
+buttonPrev.addEventListener('click',()=>{
+    if(searchPokemon >1){
+        searchPokemon -=1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click',()=>{
+    searchPokemon +=1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
